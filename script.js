30f2985ac4381ce782dc30ab23c472de570a0e04
Lewis Connolly <lewis.connolly33@gmail.com>
2023-11-23 12:39:56 +1300

    Scroll 'result' 'process' indicator resolved

--- script.js
+++ script.js
@@ -28,59 +28,30 @@ function closeOverlay() {
 }
 
 /* sidebar nav */
+document.addEventListener('DOMContentLoaded', function () {
+    let resultSection = document.getElementById('result-section');
+    let processSection = document.getElementById('process-section');
+    let resultText = document.querySelector('.side-menu li:nth-child(1) .larger-text');
+    let processText = document.querySelector('.side-menu li:nth-child(2) .larger-text');
 
-document.addEventListener("DOMContentLoaded", function () {
-    const menuLinks = document.querySelectorAll('.side-menu a');
-    const contentSections = document.querySelectorAll('.content-section');
+    window.addEventListener('scroll', function () {
+        let resultSectionOffset = resultSection.offsetTop;
+        let processSectionOffset = processSection.offsetTop;
+        let scrollPosition = window.scrollY;
 
-    // Function to check which section is currently in view
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-        );
-    }
-
-    // Function to handle link highlighting based on the section in view
-    function highlightLink() {
-        let found = false;
-
-        contentSections.forEach((section, index) => {
-            const isSectionInView = isInViewport(section);
-
-            if (isSectionInView && !found) {
-                menuLinks.forEach(link => link.classList.remove('active'));
-                menuLinks[index].classList.add('active');
-                found = true; // Set found to true to only highlight the first section in view
-            }
-        });
-
-        // Remove 'active' class from all links if no section is in view
-        if (!found) {
-            menuLinks.forEach(link => link.classList.remove('active'));
+        // Check if the user is scrolling over the Result section
+        if (scrollPosition >= resultSectionOffset && scrollPosition < processSectionOffset) {
+            resultText.classList.add('active');
+            processText.classList.remove('active');
+        } else if (scrollPosition >= processSectionOffset) { // Check if the user is scrolling over the Process section
+            resultText.classList.remove('active');
+            processText.classList.add('active');
+        } else {
+            // Default state when not scrolling over either section
+            resultText.classList.remove('active');
+            processText.classList.remove('active');
         }
-    }
-
-    // Initial highlighting on page load
-    highlightLink();
-
-    // Event listener for scroll events
-    window.addEventListener('scroll', highlightLink);
-
-    menuLinks.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - 60,
-                    behavior: 'smooth'
-                });
-            }
-        });
     });
 });
+
+
